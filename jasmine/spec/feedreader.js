36c18f884586f359eddef86ed58ef8f6ca5f8497
Erwin Rosales <look4erwin@gmail.com>
2018-08-12 23:39:19 -0700

    Implemented passing test 'RSS Feeds have URLs'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,6 +32,13 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         it('have URLs', function() {
+           for(var i = 0; i < allFeeds.length; i++) {
+             expect(allFeeds[i].url).toBeDefined();
+             expect(allFeeds[i].url).not.toBe('');
+           }
+         });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
